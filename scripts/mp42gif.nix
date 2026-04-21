8ec609eada09340a40fedb655af50a471bbbd176
macuguita <me@macuguita.com>
2026-04-21 12:33:03 +0200

    update mp42gif

--- scripts/mp42gif.nix
+++ scripts/mp42gif.nix
@@ -1,5 +1,8 @@
 { pkgs }:
 
+let
+  lib = pkgs.lib;
+in
 pkgs.writeShellScriptBin "mp42gif" ''
   VIDEO="$1"
   SPEED="''${2:-2}"
@@ -7,11 +10,11 @@ pkgs.writeShellScriptBin "mp42gif" ''
 
   PALETTE="$(mktemp /tmp/palette.XXXXXX.png)"
 
-  ${pkgs.ffmpeg}/bin/ffmpeg -i "$VIDEO" \
+  ${lib.getExe pkgs.ffmpeg} -i "$VIDEO" \
     -vf "fps=20,scale=800:-1:flags=lanczos,setpts=(1/$SPEED)*PTS,palettegen" \
     "$PALETTE"
 
-  ${pkgs.ffmpeg}/bin/ffmpeg -i "$VIDEO" -i "$PALETTE" \
+  ${lib.getExe pkgs.ffmpeg} -i "$VIDEO" -i "$PALETTE" \
     -filter_complex "[0:v]fps=20,scale=800:-1:flags=lanczos,setpts=(1/$SPEED)*PTS[v];[v][1:v]paletteuse=dither=sierra2_4a" \
     "$BASE_NAME.gif"
 
